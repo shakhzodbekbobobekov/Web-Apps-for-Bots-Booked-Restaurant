cd1d237657501dc712b6f708118069bbf910bbf1
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-07-23 13:00:25 +0500

    Added product JS and CSS code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import "./Product.css";
-
-// import "~slick-carousel/slick/slick.css";
-// import "~slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 
 function Product() {
   const settings = {
@@ -17,16 +15,179 @@ function Product() {
   };
   return (
     <div className="container">
-      <div className="carusel">
-        <Slider {...settings}>
-          <div className="product__img">
-            <img src="./images/tacos.png" alt="tacos.png" />
-          </div>
-          <div className="product__img">
-            <img src="./images/tacos.png" alt="tacos.png" />
-            <span>Lorem ipsum dolor um omnis. Ipsum.</span>
-          </div>
-        </Slider>
+      <div className="product__title">
+        <h1>Special Offers</h1>
+        <h1 className="all-product">See All</h1>
+      </div>
+      <div className="product-container">
+        <div className="carusel">
+          <Slider {...settings}>
+            <Link to="/">
+              <div className="product">
+                <div className="product__img-box">
+                  <img
+                    className="product__img"
+                    src="./images/tacos.png"
+                    alt="tacos.png"
+                  />
+                  <img
+                    className="heart-icon__product"
+                    src="./images/heart.svg"
+                    alt="heart icon product section"
+                  />
+                </div>
+
+                <div className="product__information">
+                  <h1>Lavash</h1>
+                  <div className="product__buy">
+                    <Link to="/">
+                      <img
+                        className="degree-star"
+                        src="./images/star half.svg"
+                        alt="yulduzcha"
+                      />
+                    </Link>
+                    <span>4.8 |</span>
+                    <button>4.268 Sold</button>
+                  </div>
+                  <h2>$29</h2>
+                </div>
+              </div>
+            </Link>
+
+            <Link to="/">
+              <div className="product">
+                <div className="product__img-box">
+                  <img
+                    className="product__img"
+                    src="./images/tacos.png"
+                    alt="tacos.png"
+                  />
+                  <img
+                    className="heart-icon__product"
+                    src="./images/heart.svg"
+                    alt="heart icon product section"
+                  />
+                </div>
+
+                <div className="product__information">
+                  <h1>Lavash</h1>
+                  <div className="product__buy">
+                    <Link to="/">
+                      <img
+                        className="degree-star"
+                        src="./images/star half.svg"
+                        alt="yulduzcha"
+                      />
+                    </Link>
+                    <span>4.8 |</span>
+                    <button>4.268 Sold</button>
+                  </div>
+                  <h2>$29</h2>
+                </div>
+              </div>
+            </Link>
+
+            <Link to="/">
+              <div className="product">
+                <div className="product__img-box">
+                  <img
+                    className="product__img"
+                    src="./images/tacos.png"
+                    alt="tacos.png"
+                  />
+                  <img
+                    className="heart-icon__product"
+                    src="./images/heart.svg"
+                    alt="heart icon product section"
+                  />
+                </div>
+
+                <div className="product__information">
+                  <h1>Lavash</h1>
+                  <div className="product__buy">
+                    <Link to="/">
+                      <img
+                        className="degree-star"
+                        src="./images/star half.svg"
+                        alt="yulduzcha"
+                      />
+                    </Link>
+                    <span>4.8 |</span>
+                    <button>4.268 Sold</button>
+                  </div>
+                  <h2>$29</h2>
+                </div>
+              </div>
+            </Link>
+
+            <Link to="/">
+              <div className="product">
+                <div className="product__img-box">
+                  <img
+                    className="product__img"
+                    src="./images/tacos.png"
+                    alt="tacos.png"
+                  />
+                  <img
+                    className="heart-icon__product"
+                    src="./images/heart.svg"
+                    alt="heart icon product section"
+                  />
+                </div>
+
+                <div className="product__information">
+                  <h1>Lavash</h1>
+                  <div className="product__buy">
+                    <Link to="/">
+                      <img
+                        className="degree-star"
+                        src="./images/star half.svg"
+                        alt="yulduzcha"
+                      />
+                    </Link>
+                    <span>4.8 |</span>
+                    <button>4.268 Sold</button>
+                  </div>
+                  <h2>$29</h2>
+                </div>
+              </div>
+            </Link>
+
+            <Link to="/">
+              <div className="product">
+                <div className="product__img-box">
+                  <img
+                    className="product__img"
+                    src="./images/tacos.png"
+                    alt="tacos.png"
+                  />
+                  <img
+                    className="heart-icon__product"
+                    src="./images/heart.svg"
+                    alt="heart icon product section"
+                  />
+                </div>
+
+                <div className="product__information">
+                  <h1>Lavash</h1>
+                  <div className="product__buy">
+                    <Link to="/">
+                      <img
+                        className="degree-star"
+                        src="./images/star half.svg"
+                        alt="yulduzcha"
+                      />
+                    </Link>
+                    <span>4.8 |</span>
+                    <button>4.268 Sold</button>
+                  </div>
+                  <h2>$29</h2>
+                </div>
+              </div>
+            </Link>
+          </Slider>
+        </div>
       </div>
     </div>
   );
