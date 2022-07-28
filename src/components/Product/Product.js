1bdc01c3b19b24b72a289b0639ce60c49f2e9256
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-07-29 01:32:13 +0500

    neworking icon and a little code added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./Product.module.css";
+import { Link } from "react-router-dom";
 
 function Product() {
   return (
@@ -15,15 +16,47 @@ function Product() {
       </div>
       <div className={style["product-title"]}>
         <h1>Rayhon milliy taomlar</h1>
-        <div className="product__buy">
-          <img
-            className="degree-star"
-            src="./images/star half.svg"
-            alt="yulduzcha"
-          />
-
-          <span>4.8 </span>
-        </div>
+        <img src="./images/heart.svg" alt="heart svg product" />
+      </div>
+      <div className="product__buy">
+        <img
+          className="degree-star"
+          src="./images/star half.svg"
+          alt="yulduzcha"
+        />
+        <span>4.9 (4.79 reviews)</span>
+      </div>
+      <div className={style["product-description"]}>
+        <h2>Description</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+          voluptatum perspiciatis praesentium officia provident, nesciunt porro
+          iusto possimus necessitatibus ad dicta.
+        </p>
+      </div>
+      <div className={style["social-network"]}>
+        <ul className={style["social-network__item"]}>
+          <li className={style["social-network__items"]}>
+            <Link to="/">
+              <img src="./images/icon-facebook.svg" alt="icon-facebook.svg" />
+            </Link>
+          </li>
+          <li className={style["social-network__items"]}>
+            <Link to="/">
+              <img src="./images/icon-instagram.svg" alt="icon-instagram.svg" />
+            </Link>
+          </li>
+          <li className={style["social-network__items"]}>
+            <Link to="/">
+              <img src="./images/icon-telegram.svg" alt="icon-telegram.svg" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={style.booking}>
+        <Link className={style["booking-links"]} to="/">
+          Booking
+        </Link>
       </div>
     </div>
   );
